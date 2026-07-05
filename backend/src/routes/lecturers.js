cbec9e5189aa249086b05f50fc7b5e267e679de0
Matheesha Weerawansha <matheeshaweerawansha32@gmail.com>
2026-07-05 10:29:02 +0530

    add routes

---
+++ backend/src/routes/lecturers.js
@@ -0,0 +1,10 @@
+
+import { Router } from 'express';
+import { getLecturers } from '../controllers/dataController.js';
+
+const router = Router();
+
+router.get('/', getLecturers);
+
+export default router;
+
